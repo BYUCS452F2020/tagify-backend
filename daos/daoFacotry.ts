f0026e9fab6c071d34923218cfb4f4264754c483
Brian McDaniel <mcd.briand@gmail.com>
2020-10-22 10:26:13 -0600

    remove option for test dao

--- daos/daoFacotry.ts
+++ daos/daoFacotry.ts
@@ -1,18 +1,8 @@
 import IUserDao from "./interface/IUserDao";
 import SqlUserDao from "./sql/SqlUserDao";
-import TestUserDao from "./test/TestUserDao";
-
-let impl = "sql";
 
 export const UserDaoFactory = class {
     static create() : IUserDao {
-        switch (impl) {
-            case "test":
-                return new TestUserDao();
-            case "sql":
-                return new SqlUserDao();
-            default:
-                throw new Error("Unknown UserDao implementation");
-        }
+        return new SqlUserDao();
     }
 }
